6def83c2dd81a592cebc40486d4d840cf9090c0b
Syre Musk <favourforchrist91@gmail.com>
2022-06-26 22:12:20 +0100

    commit

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('about/', include('about.urls')),
     path('projects/', include('projects.urls')),
